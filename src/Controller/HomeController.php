c7a4061d4f015c4244bdb9af059c6d76f3e2ff07
Benjamin Carpentier <benjamincarpentier01@gmail.com>
2023-05-02 20:02:34 +0200

    feat:header et footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,23 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $gites = [
+            [
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13/01/2023',
+               'datefin' => '08/04/2023',
+               'image' => 'https://www.gitesdefrance35.com/photos/gites35/G/photo33/gite-roz-landrieux-bretagne--ref--35G12036_440_791.jpg'
+            ],
+            [
+                'nom' => 'Le palais Breton',
+                'prix' => '95',
+                'datedebut' => '13/01/2023',
+                'datefin' => '08/04/2023',
+                'image' => 'https://www.gitesdefrance35.com/photos/gites35/G/photo33/gite-roz-landrieux-bretagne--ref--35G12036_440_791.jpg'
+            ]
+        ];
+
+        return $this->render('home/index.html.twig', ['gites' => $gites]);
     }
 }
