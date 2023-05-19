c91b46d4a0dab54f9319289d15d0e10803da1f56
Benjamin Carpentier <benjamincarpentier01@gmail.com>
2023-05-19 07:45:04 +0200

    style: main page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,19 +13,142 @@ class HomeController extends AbstractController
     {
         $gites = [
             [
+                'id' => 1,
                'nom' => 'Le palais Breton',
                'prix' => '95',
-               'datedebut' => '13/01/2023',
-               'datefin' => '08/04/2023',
-               'image' => 'https://www.gitesdefrance35.com/photos/gites35/G/photo33/gite-roz-landrieux-bretagne--ref--35G12036_440_791.jpg'
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
             ],
             [
+                'id' => 2,
                 'nom' => 'Le palais Breton',
                 'prix' => '95',
-                'datedebut' => '13/01/2023',
-                'datefin' => '08/04/2023',
-                'image' => 'https://www.gitesdefrance35.com/photos/gites35/G/photo33/gite-roz-landrieux-bretagne--ref--35G12036_440_791.jpg'
+                'datedebut' => '13.01.2023',
+                'datefin' => '08.04.2023',
+                'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
+            ],
+            [
+                'id' => 1,
+               'nom' => 'Le palais Breton',
+               'prix' => '95',
+               'datedebut' => '13.01.2023',
+               'datefin' => '08.04.2023',
+               'image' => 'https://www.maisonapart.com/images/auto/640-480-c/20220311_173419_istock-471826199.jpg'
             ]
+            
         ];
 
         return $this->render('home/index.html.twig', ['gites' => $gites]);
